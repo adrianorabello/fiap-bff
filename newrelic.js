4ccabf84505c13c9d173397a4fb388d7cbeb1c54
adrianorabello <adrianorabello@icloud.com>
2026-02-01 16:15:55 +0000

    passando a chave pro env

--- newrelic.js
+++ newrelic.js
@@ -1,7 +1,7 @@
 'use strict';
 exports.config = {
   app_name: [process.env.NEW_RELIC_APP_NAME || 'My Application'],
-  license_key: process.env.NEW_RELIC_LICENSE_KEY,
+  license_key: 'eu01xxc1c809643d2937d628ee79e701FFFFNRAL',
   logging: {
     level: 'info'
   }
